90ce7a0fb5fe5a26a0e8d06209b6c99b65fbe8ba
blackweeed <blackweeeed@gmail.com>
2023-03-27 20:23:13 +0200

    Initial

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getGoals,
+  deleteGoal,
+  updateGoal,
+  setGoal,
+} = require("../controllers/goalController");
+
+router.route("/").get(getGoals).post(setGoal);
+router.route("/id").put(updateGoal).delete(deleteGoal);
+
+module.exports = router;
